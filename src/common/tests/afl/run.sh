414e19c94008846512ac789eeecdca4588979cbd
Jan Michalski <jan-marian.michalski@hpe.com>
2025-01-07 15:26:29 +0000

    XXX

--- src/common/tests/afl/run.sh
+++ src/common/tests/afl/run.sh
@@ -5,7 +5,8 @@ export PMEMOBJ_CONF="sds.at_create=0"
 
 # KNOWN ISSUES:
 # - unknown key: 36, 42, 84, 90
-# - SIGSEGV: 39, 45, 87, 93
+# - SIGSEGV: 39, 45, 87, 93 all basically the same and using BTR_FEAT_EMBEDDED
+#   - the issue goes away when the BTR_FEAT_EMBEDDED feature is turned off
 
 gdbserver localhost:2345 ./btree --from-file testcase_dir/$1.bin
 
