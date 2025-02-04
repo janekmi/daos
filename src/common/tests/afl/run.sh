56a32c41ce63e6ff1e45019109da302075d52a84
Jan Michalski <jan-marian.michalski@hpe.com>
2025-02-04 22:08:24 +0000

    run.sh - use the regular btree binary

--- src/common/tests/afl/run.sh
+++ src/common/tests/afl/run.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH=/opt/daos/prereq/release/pmdk/lib/pmdk_debug:/opt/daos/lib64
 export PMEMOBJ_CONF="sds.at_create=0"
+BIN=/home/beside/work/daos-stack/daos/build/debug/afl-gcc/src/common/tests/btree
 
 # KNOWN ISSUES:
 # - unknown key: 36, 42, 84, 90
 # - SIGSEGV: 39, 45, 87, 93 all basically the same and using BTR_FEAT_EMBEDDED
 #   - the issue goes away when the BTR_FEAT_EMBEDDED feature is turned off
+# - patch for all of these: https://github.com/daos-stack/daos/pull/15815
 
-gdbserver localhost:2345 ./btree --from-file testcase_dir/$1.bin
+# gdbserver localhost:2345 $BIN --from-file testcase_dir/$1.bin
 
-# set -e
-# for bin in testcase_dir/*.bin; do
-#     echo "./btree --from-file $bin"
-#     ./btree --from-file $bin
-# done
+set -e
+for bin in testcase_dir/*.bin; do
+    echo "$BIN --from-file $bin"
+    $BIN --from-file $bin
+done
