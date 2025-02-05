e68a7317e7ddbb3f9985348308c9d8b9b5f2db8e
Jan Michalski <jan-marian.michalski@hpe.com>
2025-02-05 00:58:10 +0000

    Script to run afl-cmin

---
+++ src/common/tests/afl/afl.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PATH=/home/beside/work/AFL:$PATH
+BIN=/home/beside/work/daos-stack/daos/build/debug/afl-gcc/src/common/tests/btree
+
+afl-cmin -i testcase_dir -o findings_dir $BIN --from-file @@
+
+# afl-cmin produces no log on crash/failure. The line could be helpful in this case.
+# Copied directly from the afl-cmin script.
+# AFL_CMIN_ALLOW_ANY=1 "/home/beside/work/AFL/afl-showmap" -m "100" -t "none" -o "findings_dir/.traces/.run_test" -A "findings_dir/.traces/.cur_input" -- ./btree --from-file @@
