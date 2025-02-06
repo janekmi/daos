7a9838b2d26941d34b609278938ac78871727c46
Jan Michalski <jan-marian.michalski@hpe.com>
2025-02-06 15:41:59 +0000

    run.sh with AFL-like memory limit

--- src/common/tests/afl/run.sh
+++ src/common/tests/afl/run.sh
@@ -9,10 +9,12 @@ BIN=/home/beside/work/daos-stack/daos/build/debug/afl-gcc/src/common/tests/btree
 #   - the issue goes away when the BTR_FEAT_EMBEDDED feature is turned off
 # - patch for all of these: https://github.com/daos-stack/daos/pull/15815
 
-# gdbserver localhost:2345 $BIN --from-file $1
+LIMIT_MB=50
+GDB="gdbserver localhost:2345"
+(ulimit -Sv $[LIMIT_MB << 10]; $GDB $BIN --from-file $1)
 
-set -e
-for bin in testcase_dir/*.bin; do
-    echo "$BIN --from-file $bin"
-    $BIN --from-file $bin
-done
+# set -e
+# for bin in testcase_dir/*.bin; do
+#     echo "$BIN --from-file $bin"
+#     $BIN --from-file $bin
+# done
