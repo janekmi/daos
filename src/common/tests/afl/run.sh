3bc0d8c2ab5d628024ec692a72824d7a359c2cdf
Jan Michalski <jan-marian.michalski@hpe.com>
2025-02-06 17:00:08 +0000

    run.sh improvements

--- src/common/tests/afl/run.sh
+++ src/common/tests/afl/run.sh
@@ -9,9 +9,11 @@ BIN=/home/beside/work/daos-stack/daos/build/debug/afl-gcc/src/common/tests/btree
 #   - the issue goes away when the BTR_FEAT_EMBEDDED feature is turned off
 # - patch for all of these: https://github.com/daos-stack/daos/pull/15815
 
-LIMIT_MB=50
-GDB="gdbserver localhost:2345"
-(ulimit -Sv $[LIMIT_MB << 10]; $GDB $BIN --from-file $1)
+LIMIT_MB=100
+# GDB="gdbserver localhost:2345"
+# PRELOAD="/home/beside/work/AFL/libdislocator/libdislocator.so"
+(ulimit -Sv $[LIMIT_MB << 10]; LD_PRELOAD=$PRELOAD $GDB $BIN --from-file $1)
+# gdb $BIN $1
 
 # set -e
 # for bin in testcase_dir/*.bin; do
