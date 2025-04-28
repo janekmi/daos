4c7a5691d9b0f340c826f093cf2d8b8e22de586e
Jan Michalski <jan-marian.michalski@hpe.com>
2025-04-28 10:45:42 +0100

    DLCK: vos_iterate() issue repro refine
    Signed-off-by: Jan Michalski <jan-marian.michalski@hpe.com>

--- src/utils/dlck/tests/dlck_ut_helper.c
+++ src/utils/dlck/tests/dlck_ut_helper.c
@@ -19,7 +19,7 @@
 #include <daos_srv/dtx_srv.h>
 
 struct vos_test_ctx {
-	char         *tc_po_name;
+	// char         *tc_po_name;
 	uuid_t        tc_po_uuid;
 	uuid_t        tc_co_uuid;
 	daos_handle_t tc_po_hdl;
@@ -27,37 +27,15 @@ struct vos_test_ctx {
 	int           tc_step;
 };
 
-struct io_test_args {
-	struct vos_test_ctx ctx;
-	daos_unit_oid_t     oid;
-	/* Optional addn container create params */
-	uuid_t              addn_co_uuid;
-	daos_handle_t       addn_co;
-	/* testing flags, see vts_test_flags */
-	daos_epoch_t        epr_lo;
-	unsigned long       ta_flags;
-	const char         *dkey;
-	const char         *akey;
-	void               *custom;
-	enum daos_otype_t   otype;
-	int                 akey_size;
-	int                 dkey_size;
-	int                 co_create_step;
-	bool                checkpoint;
-	bool                no_replay;
-	bool                fail_replay;
-	bool                fail_checkpoint;
-};
+#define VPOOL_SIZE  (1024 * 1024 * 10) /** 10MiB */
 
-#define SRAND_SEED  1743171631
-
-#define VPOOL_SIZE  (1024 * 1024 * 10)
-
+#define VOS_PATH    "/mnt/daos"
 #define PO_UUID_STR "a367beed-8857-461c-a532-92ca618e589c"
+#define POOL_NAME   "vpool.0"
+#define POOL_PATH   VOS_PATH "/" PO_UUID_STR "/" POOL_NAME
 
-static const char vos_path[]      = "/mnt/daos";
 static const char Po_uuid_str[]   = PO_UUID_STR;
-static const char Co_uuid_str[]  = "0faccb2b-d498-49d4-aeef-0668e929e919";
+static const char Co_uuid_str[]   = "0faccb2b-d498-49d4-aeef-0668e929e919";
 static const char Dti1_uuid_str[] = "0faccb2b-d498-49d4-aeee-0668e929e000";
 static const char Dti2_uuid_str[] = "525c6a15-8bc9-4918-a8fa-98b959ce6575";
 
@@ -66,58 +44,40 @@ test_cleanup()
 {
 	int rc;
 
-	rc = unlink("/mnt/daos/" PO_UUID_STR "/vpool.0");
+	rc = unlink(POOL_PATH);
 	assert_true(rc == 0 || (rc == -1 && errno == ENOENT));
-	rc = rmdir("/mnt/daos/" PO_UUID_STR);
+	rc = rmdir(VOS_PATH "/" PO_UUID_STR);
 	assert_true(rc == 0 || (rc == -1 && errno == ENOENT));
-	rc = unlink("/mnt/daos/daos_sys/sys_db");
+	rc = unlink(VOS_PATH "/daos_sys/sys_db");
 	assert_true(rc == 0 || (rc == -1 && errno == ENOENT));
-	rc = rmdir("/mnt/daos/daos_sys");
+	rc = rmdir(VOS_PATH "/daos_sys");
 	assert_true(rc == 0 || (rc == -1 && errno == ENOENT));
-}
-
-int
-iter_cb_nop(daos_handle_t ih, vos_iter_entry_t *entry, vos_iter_type_t type,
-	    vos_iter_param_t *param, void *cb_arg, unsigned int *acts)
-{
-	return 0;
 }
 
 static void
-run_all_tests(void)
+test_setup(struct vos_test_ctx *tcx)
 {
-	daos_size_t          psize     = VPOOL_SIZE;
-	daos_size_t          meta_size = 0;
-	uint64_t             dkey_buf  = 1;
-	daos_key_t           dkey;
-	daos_iod_t           iod      = {0};
-	uint64_t             akey_buf = 2;
-	daos_key_t           akey;
-	char                *value = "Aloha";
-	d_sg_list_t          sgl;
-	int                  rc;
-	char                *path;
-
-	struct io_test_args  args;
-	struct vos_test_ctx *tcx = &args.ctx;
-	srand(SRAND_SEED);
+	daos_size_t psize     = VPOOL_SIZE;
+	daos_size_t meta_size = 0;
+	// char       *path;
+	int         rc;
 
 	rc = uuid_parse(Po_uuid_str, tcx->tc_po_uuid);
 	assert_int_equal(rc, 0);
 	rc = uuid_parse(Co_uuid_str, tcx->tc_co_uuid);
 	assert_int_equal(rc, 0);
 
-	rc = asprintf(&path, "%s/%s", vos_path, Po_uuid_str);
-	assert_int_not_equal(rc, -1);
+	// rc = asprintf(&path, "%s/%s", vos_path, Po_uuid_str);
+	// assert_int_not_equal(rc, -1);
 
-	rc = mkdir(path, 0777);
+	rc = mkdir(VOS_PATH "/" PO_UUID_STR, 0777);
 	assert_int_equal(rc, 0);
 
-	rc = asprintf(&tcx->tc_po_name, "%s/%s/vpool.0", vos_path, Po_uuid_str);
-	assert_int_not_equal(rc, -1);
+	// rc = asprintf(&tcx->tc_po_name, "%s/%s/vpool.0", vos_path, Po_uuid_str);
+	// assert_int_not_equal(rc, -1);
 
-	rc = vos_pool_create(tcx->tc_po_name, tcx->tc_po_uuid, psize, psize, meta_size,
-			     0 /* flags */, 0 /* version */, &tcx->tc_po_hdl);
+	rc = vos_pool_create(POOL_PATH, tcx->tc_po_uuid, psize, psize, meta_size, 0 /* flags */,
+			     0 /* version */, &tcx->tc_po_hdl);
 	assert_int_equal(rc, 0);
 
 	rc = vos_cont_create(tcx->tc_po_hdl, tcx->tc_co_uuid);
@@ -125,28 +85,91 @@ run_all_tests(void)
 
 	rc = vos_cont_open(tcx->tc_po_hdl, tcx->tc_co_uuid, &tcx->tc_co_hdl);
 	assert_int_equal(rc, 0);
+}
 
-	struct dtx_handle *dth;
-	struct dtx_id      dti1       = {0};
-	struct dtx_id      dti2       = {0};
-	daos_unit_oid_t    leader_oid = {0};
+static void
+test_teardown(struct vos_test_ctx *tcx)
+{
+	int rc;
+
+	rc = vos_cont_close(tcx->tc_co_hdl);
+	assert_int_equal(rc, 0);
+
+	rc = vos_pool_close(tcx->tc_po_hdl);
+	assert_int_equal(rc, 0);
+}
+
+static void
+simple_dtx(daos_handle_t coh, const char *dti_uuid_str, daos_unit_oid_t oid, daos_key_t *dkey,
+	   daos_iod_t *iod, d_sg_list_t *sgl)
+{
+	struct dtx_id      dti        = {0};
 	struct dtx_epoch   epoch      = {0};
-	epoch.oe_value                = d_hlc_get();
-	uint32_t flags                = 0;
+	daos_unit_oid_t    leader_oid = {0};
+	uint32_t           flags      = 0;
+	struct dtx_handle *dth;
+	int                rc;
 
-	rc = uuid_parse(Dti_uuid_str, dti1.dti_uuid);
+	rc = uuid_parse(dti_uuid_str, dti.dti_uuid);
 	assert_int_equal(rc, 0);
-	dti1.dti_hlc = d_hlc_get();
+	dti.dti_hlc = d_hlc_get();
 
-	Dti_uuid_str[UUID_STR_LEN - 2] = '1';
-	rc                             = uuid_parse(Dti_uuid_str, dti2.dti_uuid);
-	assert_int_equal(rc, 0);
-	dti2.dti_hlc = d_hlc_get();
+	epoch.oe_value = d_hlc_get();
 
-	rc =
-	    dtx_begin(tcx->tc_co_hdl, &dti1, &epoch, 1, 0, &leader_oid, NULL, 0, flags, NULL, &dth);
+	rc = dtx_begin(coh, &dti, &epoch, 1, 0, &leader_oid, NULL, 0, flags, NULL, &dth);
 	assert_int_equal(rc, 0);
 
+	rc = dtx_sub_init(dth, &oid, 0);
+	assert_int_equal(rc, 0);
+
+	rc = vos_obj_update_ex(coh, oid, 0, 0, 0, dkey, 1, iod, NULL, sgl, dth);
+	assert_int_equal(rc, 0);
+
+	rc = dtx_end(dth, NULL, DER_SUCCESS);
+	assert_int_equal(rc, 0);
+
+	printf("[DTX %s] Commit? (y or [n]): ", dti_uuid_str);
+	char op = getchar();
+	if (op == 'y') {
+		rc = vos_dtx_commit(coh, &dti, 1, true, NULL);
+		assert_int_equal(rc, 1); /** total number of committed */
+	}
+	if (op != '\n') {
+		(void)getchar(); /** consume the leftover \n */
+	}
+}
+
+static int
+iter_cb_printf(daos_handle_t ih, vos_iter_entry_t *entry, vos_iter_type_t type,
+	       vos_iter_param_t *param, void *cb_arg, unsigned int *acts)
+{
+	assert_int_equal(type, VOS_ITER_OBJ);
+
+	printf("- oid=(lo=%" PRIu64 ", hi=%" PRIu64 ")\n", entry->ie_oid.id_pub.lo,
+	       entry->ie_oid.id_pub.hi);
+
+	return 0;
+}
+
+static void
+run_all_tests(void)
+{
+	daos_unit_oid_t     oid      = {0};
+	uint64_t            dkey_buf = 1;
+	daos_key_t          dkey;
+	uint64_t            akey_buf = 2;
+	daos_key_t          akey;
+	daos_iod_t          iod   = {0};
+	char               *value = "Aloha";
+	d_sg_list_t         sgl;
+	int                 rc;
+
+	struct vos_test_ctx tcx;
+
+	/** prepare a container */
+	test_setup(&tcx);
+
+	/** prepare DKEY, AKEY, IOD and SGL */
 	d_iov_set(&dkey, (void *)&dkey_buf, sizeof(dkey_buf));
 	d_iov_set(&akey, (void *)&akey_buf, sizeof(akey_buf));
 	iod.iod_name  = akey;
@@ -158,57 +181,27 @@ run_all_tests(void)
 	assert_int_equal(rc, 0);
 	d_iov_set(&sgl.sg_iovs[0], (void *)value, iod.iod_size);
 
-	rc = dtx_sub_init(dth, &args.oid, 0);
-	assert_int_equal(rc, 0);
+	/** 1st DTX */
+	simple_dtx(tcx.tc_co_hdl, Dti1_uuid_str, oid, &dkey, &iod, &sgl);
 
-	rc = vos_obj_update_ex(args.ctx.tc_co_hdl, args.oid, 0, 0, 0, &dkey, 1, &iod, NULL, &sgl,
-			       dth);
-	assert_int_equal(rc, 0);
+	/** 2nd DTX (just a different OID) */
+	oid.id_pub.lo = 1;
+	simple_dtx(tcx.tc_co_hdl, Dti2_uuid_str, oid, &dkey, &iod, &sgl);
 
-	rc = dtx_end(dth, NULL, DER_SUCCESS);
-	assert_int_equal(rc, 0);
-
-	/** XXX */
-	rc =
-	    dtx_begin(tcx->tc_co_hdl, &dti2, &epoch, 1, 0, &leader_oid, NULL, 0, flags, NULL, &dth);
-	assert_int_equal(rc, 0);
-
-	rc = dtx_sub_init(dth, &args.oid, 0);
-	assert_int_equal(rc, 0);
-
-	args.oid.id_pub.lo = 1;
-
-	rc = vos_obj_update_ex(args.ctx.tc_co_hdl, args.oid, 0, 0, 0, &dkey, 1, &iod, NULL, &sgl,
-			       dth);
-	assert_int_equal(rc, 0);
-
-	rc = dtx_end(dth, NULL, DER_SUCCESS);
-	assert_int_equal(rc, 0);
-
-	rc = vos_dtx_commit(args.ctx.tc_co_hdl, &dti1, 1, true, NULL);
-	assert_int_equal(rc, 1); /** total number of committed */
-	/** XXX */
-
-	// rc = vos_dtx_commit(args.ctx.tc_co_hdl, &dti2, 1, true, NULL);
-	// assert_int_equal(rc, 1); /** total number of committed */
-
+	/** iterate over OBJs */
 	vos_iter_param_t        param   = {0};
 	struct vos_iter_anchors anchors = {0};
 
-	param.ip_hdl        = args.ctx.tc_co_hdl;
+	param.ip_hdl        = tcx.tc_co_hdl;
 	param.ip_epr.epr_hi = DAOS_EPOCH_MAX;
 
-	rc = vos_iterate(&param, VOS_ITER_OBJ, false, &anchors, iter_cb_nop, iter_cb_nop, NULL,
-			 NULL);
-	assert_int_equal(rc, 0);
+	printf("vos_iterate()...\n");
+	rc = vos_iterate(&param, VOS_ITER_OBJ, false, &anchors, iter_cb_printf, NULL, NULL, NULL);
+	printf("vos_iterate() -> rc = %d\n", rc);
 
+	/** quick teardown */
 	d_sgl_fini(&sgl, false);
-
-	rc = vos_cont_close(tcx->tc_co_hdl);
-	assert_int_equal(rc, 0);
-
-	rc = vos_pool_close(tcx->tc_po_hdl);
-	assert_int_equal(rc, 0);
+	test_teardown(&tcx);
 }
 
 int
@@ -226,7 +219,7 @@ main(int argc, char **argv)
 
 	test_cleanup();
 
-	rc = vos_self_init(vos_path, true, BIO_STANDALONE_TGT_ID);
+	rc = vos_self_init(VOS_PATH, true, BIO_STANDALONE_TGT_ID);
 	if (rc) {
 		print_error("Error initializing VOS instance\n");
 		goto exit_0;
