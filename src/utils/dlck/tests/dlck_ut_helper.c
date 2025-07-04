28a22ffffa06fd67ca01e3297aceeaf412eb2448
Jan Michalski <jan-marian.michalski@hpe.com>
2025-07-04 17:44:25 +0000

    dlck_ut_helper: add a simple EV update
    Signed-off-by: Jan Michalski <jan-marian.michalski@hpe.com>

--- src/utils/dlck/tests/dlck_ut_helper.c
+++ src/utils/dlck/tests/dlck_ut_helper.c
@@ -34,13 +34,14 @@ struct vos_test_ctx {
 
 #define VPOOL_SIZE (1024 * 1024 * 10) /** 10MiB */
 
-static const char Po_uuid_str[]   = PO_UUID_STR;
 static const char Co_uuid_str[]   = "0faccb2b-d498-49d4-aeef-0668e929e919";
 static const char Dti1_uuid_str[] = "0faccb2b-d498-49d4-aeee-0668e929e000";
 static const char Dti2_uuid_str[] = "525c6a15-8bc9-4918-a8fa-98b959ce6575";
+static const char Dti3_uuid_str[] = "25c6a150-8bc9-4918-a8fa-b959ce657503";
 
 struct xstream_arg {
-	struct dlck_args_engine *args;
+	struct dlck_args_engine *args_engine;
+	struct dlck_args_files  *args_files;
 	struct dlck_engine      *engine;
 	struct dlck_xstream     *xs;
 	struct dlck_file        *file;
@@ -141,6 +142,7 @@ run_all_tests(daos_handle_t poh)
 	uint64_t            akey_buf = 2;
 	daos_key_t          akey;
 	daos_iod_t          iod   = {0};
+	daos_recx_t         rex   = {0};
 	char               *value = "Aloha";
 	d_sg_list_t         sgl;
 	int                 rc;
@@ -171,6 +173,14 @@ run_all_tests(daos_handle_t poh)
 	oid.id_pub.lo = 1;
 	simple_dtx(tcx.tc_co_hdl, Dti2_uuid_str, oid, &dkey, &iod, &sgl);
 
+	/** 3rd DTX (EV) */
+	oid.id_pub.lo = 2;
+	rex.rx_idx    = 0;
+	rex.rx_nr     = 1;
+	iod.iod_type  = DAOS_IOD_ARRAY;
+	iod.iod_recxs = &rex;
+	simple_dtx(tcx.tc_co_hdl, Dti3_uuid_str, oid, &dkey, &iod, &sgl);
+
 	/** iterate over OBJs */
 	vos_iter_param_t        param   = {0};
 	struct vos_iter_anchors anchors = {0};
@@ -194,12 +204,9 @@ static void
 exec_one(void *arg)
 {
 	struct xstream_arg *xa = arg;
+	struct dlck_file   *file;
 	daos_handle_t       poh;
 	int rc;
-
-	if (xa->xs->tgt_id != 0) {
-		return;
-	}
 
 	rc = dlck_engine_xstream_init(xa->xs);
 	if (rc != 0) {
@@ -207,23 +214,29 @@ exec_one(void *arg)
 		return;
 	}
 
-	ABT_mutex_lock(xa->engine->open_mtx);
-	rc = dlck_pool_open(xa->args->storage_path, xa->file, xa->xs->tgt_id, &poh);
-	ABT_mutex_unlock(xa->engine->open_mtx);
-	if (rc != 0) {
-		xa->rc = rc;
-		return;
-	}
+	d_list_for_each_entry(file, &xa->args_files->list, link) {
+		if ((file->targets & (1 << xa->xs->tgt_id)) == 0) {
+			continue;
+		}
 
-	run_all_tests(poh);
+		ABT_mutex_lock(xa->engine->open_mtx);
+		rc = dlck_pool_open(xa->args_engine->storage_path, file, xa->xs->tgt_id, &poh);
+		ABT_mutex_unlock(xa->engine->open_mtx);
+		if (rc != 0) {
+			xa->rc = rc;
+			return;
+		}
 
-	ABT_mutex_lock(xa->engine->open_mtx);
-	rc = vos_pool_close(poh);
-	if (rc != 0) {
-		xa->rc = rc;
-		return;
+		run_all_tests(poh);
+
+		ABT_mutex_lock(xa->engine->open_mtx);
+		rc = vos_pool_close(poh);
+		if (rc != 0) {
+			xa->rc = rc;
+			return;
+		}
+		ABT_mutex_unlock(xa->engine->open_mtx);
 	}
-	ABT_mutex_unlock(xa->engine->open_mtx);
 
 	rc = dlck_engine_xstream_fini(xa->xs);
 	if (rc != 0) {
@@ -243,7 +256,8 @@ arg_alloc(struct dlck_engine *engine, int idx, void *input_arg, void **output_ar
 		return ENOMEM;
 	}
 
-	xa->args   = input_xa->args;
+	xa->args_engine = input_xa->args_engine;
+	xa->args_files  = input_xa->args_files;
 	xa->engine = engine;
 	xa->xs     = &engine->xss[idx];
 	xa->file   = input_xa->file;
@@ -262,25 +276,54 @@ arg_free(void **arg)
 	return 0;
 }
 
+extern struct argp       argp_file;
+extern struct argp       argp_engine;
+
+static struct argp_child children[] = {{&argp_file}, {&argp_engine}, {0}};
+
+struct dlck_helper_args {
+	struct dlck_args_files  files;
+	struct dlck_args_engine engine;
+};
+
+error_t
+parser(int key, char *arg, struct argp_state *state)
+{
+	struct dlck_helper_args *args = state->input;
+
+	/** state changes */
+	switch (key) {
+	case ARGP_KEY_INIT:
+		state->child_inputs[0] = &args->files;
+		state->child_inputs[1] = &args->engine;
+		break;
+	}
+
+	return ARGP_ERR_UNKNOWN;
+}
+
+static struct argp argp = {NULL, parser, NULL /** usage */, NULL, children};
+
 int
 main(int argc, char **argv)
 {
-	struct dlck_args_engine args   = {0};
+	struct dlck_helper_args args   = {0};
 	struct dlck_engine     *engine = NULL;
 	struct xstream_arg      xa     = {0};
+	struct dlck_file       *file;
 	int                     rc;
 
-	argp_parse(&argp_engine, argc, argv, 0, 0, &args);
+	argp_parse(&argp, argc, argv, 0, 0, &args);
 
-	xa.args = &args;
+	xa.args_files  = &args.files;
+	xa.args_engine = &args.engine;
 
-	rc = parse_file(Po_uuid_str, NULL, &xa.file);
-	assert_int_equal(rc, 0);
+	d_list_for_each_entry(file, &args.files.list, link) {
+		rc = dlck_pool_mkdir(args.engine.storage_path, file->po_uuid);
+		assert_int_equal(rc, 0);
+	}
 
-	rc = dlck_pool_mkdir(args.storage_path, xa.file->po_uuid);
-	assert_int_equal(rc, 0);
-
-	rc = dlck_engine_start(&args, &engine);
+	rc = dlck_engine_start(&args.engine, &engine);
 	if (rc != 0) {
 		return rc;
 	}
